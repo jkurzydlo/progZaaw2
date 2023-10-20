5a89f0b03ab67991c18f916850322644aa2de1ba
Jacek <jacek.kurzydlo@protonmail.ch>
2023-10-20 13:41:07 +0200

    Dodano mechanizm dodawania elementu do drzewa

--- BST.hpp
+++ BST.hpp
@@ -15,7 +15,23 @@ public:
         dane = wartosc;
         lewe = prawe = NULL;
     }
-   
+    BST* wstaw(BST* korzen, int wartosc){
+
+    //Jeśli drzewo jest puste dodaj korzeń    
+    if (!korzen) 
+    return new BST(wartosc);
+    
+    //Jesli wartosc jest wieksza wstaw do prawego poddrzewa
+    if (wartosc > korzen->dane) 
+        korzen->prawe = wstaw(korzen->prawe, wartosc);
+    
+    //Jesli wartosc jest mniejsza wstaw do lewgo poddrzewa
+    else if (wartosc < korzen->dane) 
+        korzen->lewe = wstaw(korzen->lewe, wartosc);
+    
+    return korzen;
+    }
+ 
 };
  
 
--- main.cpp
+++ main.cpp
@@ -1,4 +1,12 @@
 #include "BST.hpp"
 int main(){
+    BST drzewo, *korzen = NULL;
+    korzen = drzewo.wstaw(korzen,50);
+
+    drzewo.wstaw(korzen,10);
+    drzewo.wstaw(korzen,15);
+    drzewo.wstaw(korzen,20);
+
+
 
 }
