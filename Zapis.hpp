a0382957595cf7e56b5c21c1f7d90327c79d63c2
Jacek <jacek.kurzydlo@protonmail.ch>
2023-10-29 18:03:10 +0100

    Dodano zapis i odczyt z pliku tekstowego

--- Zapis.hpp
+++ Zapis.hpp
@@ -15,10 +15,17 @@ class Zapis{
     dodajDoZapisu(wezel->getPrawe());// Przejdz prawe poddrzewo
 
 }
+    void dodajDoZapisuTxt(int* wezly, int rozmiar){
+      //std::cout<<sizeof(wezly)/sizeof(wezly[0]);
+      for(int i =0;i< rozmiar;i++){
+        zapis<<wezly[i]<<" ";
+      }
+    }
+
   std::ofstream zapis;
   std::ifstream odczyt;
   std::string sciezka;
-  int do_zapisu=0,rozmiar=0;
+  int do_zapisu=0;
 
     public:
     ~Zapis(){
@@ -29,29 +36,46 @@ class Zapis{
     
 
     void zapiszDoPliku(BST* korzen){
-        zapis.open(sciezka,std::ios::binary);
-
+       zapis.open(sciezka,std::ios::binary);
         dodajDoZapisu(korzen);    
         zapis.close();
-
     }
-    BST* wczytajZPliku(BST& drzewo, int rozmiar){
+
+    void zapiszDoPlikuTxt(int* wezly, int rozmiar){
+      zapis.open(sciezka);
+      dodajDoZapisuTxt(wezly,rozmiar);
+      zapis.close();
+    }
+
+    BST* wczytajZPliku(BST& drzewo, BST*korzen, int rozmiar, bool tekstowo){
       BST temp_drzewo;
-      BST* korzen = NULL;
-
-
+      BST* temp_korzen = NULL;
+      if(!tekstowo){
       odczyt.open(sciezka,std::ios::binary);
 
       int* bin = new int[rozmiar];
       //wczytuje kolejne liczby, sizeof(bin[0]*rozmiar) oznacza rozmiar całej tablicy (dla 4 elementów -> 4*4(rozmiar inta) bajty)
       odczyt.read((char*)bin,sizeof(bin[0])*rozmiar);
 
-      korzen=drzewo.wstaw(korzen,bin[0]);
-      for(int i =1;i<rozmiar;i++)temp_drzewo.wstaw(korzen,bin[i]);
+      korzen=drzewo.wstaw(temp_korzen,bin[0]);
+      for(int i =1;i<rozmiar;i++)temp_drzewo.wstaw(temp_korzen,bin[i]);
 
+      }else{
+        odczyt.open(sciezka);
+
+        string element;
+        while (std::getline(odczyt,element,' '))
+        {
+          //zmiana string na int
+          std::cout<<std::stoi(element);
+           drzewo.wstaw(korzen, std::stoi(element));
+        }
+        
+       
+      }
       return korzen;
       odczyt.close();
-      //drzewo.wstaw()
+      
     }
     
 };
--- main.cpp
+++ main.cpp
@@ -8,11 +8,26 @@ int main(){
     drzewo.wstaw(korzen,20);
     drzewo.wstaw(korzen,12);
     drzewo.usunElement(korzen,20);
-    drzewo.inorder(korzen);
+   // drzewo.inorder(korzen);
     //drzewo.preorder(korzen);
     
     Zapis z1("C:\\Users\\Jacek\\Desktop\\zapis1.bin");
+    Zapis z2("C:\\Users\\Jacek\\Desktop\\zapis1.txt");
+    
     z1.zapiszDoPliku(korzen);
     BST drzewo2;
-    drzewo2.inorder(z1.wczytajZPliku(drzewo,4));
+    //drzewo2.inorder(z1.wczytajZPliku(drzewo,korzen,4,false));
+
+    BST drzewo3, *korzen2=NULL;
+    korzen2 = drzewo.wstaw(korzen2,11);
+    drzewo3.wstaw(korzen2,22);
+    drzewo3.wstaw(korzen2,33);
+    drzewo3.wstaw(korzen2,2);
+
+    int tab[5]{1,3,6,8,7};
+
+    //sizeof(tab)/sizeof(tab[0]) - ilość elementów w tablicy
+    z2.zapiszDoPlikuTxt(tab,sizeof(tab)/sizeof(tab[0]));
+    (z2.wczytajZPliku(drzewo3,korzen2,5,true));
+    drzewo3.inorder(korzen2);
 }
